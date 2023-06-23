aa6983f7c452f3305fd9e34e986a13f1d46500d0
Ivan Rodrigo Linares Pineda <ivanlinares@gmail.com>
2023-06-23 13:07:49 -0600

    Add files via upload

---
+++ remove_Acad.ps1
@@ -0,0 +1,16 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+ {
+  Echo "This script needs to be run As Admin"
+  Break
+ }
+Get-Item ${env:programfiles}\Autodesk* | Remove-Item -Force  
+Get-Item ${env:programfiles}'\Common Files'\Autodesk* | Remove-Item -Force  
+Get-Item ${env:programfiles(x86)}\Autodesk* | Remove-Item -Force  
+Get-Item ${env:programfiles(x86)}'\Common Files'\Autodesk* | Remove-Item -Force  
+Get-Item ${env:programdata}\Autodesk* | Remove-Item -Force  
+Get-Item ${env:programdata}\FLEXnet* | Remove-Item -Force  
+Get-Item ${env:appdata}\Autodesk* | Remove-Item -Force  
+Get-Item ${env:localappdata}\Autodesk* | Remove-Item -Force  
+Get-Item HKLM:\SOFTWARE\Autodesk | Remove-Item -Force -Verbose  
+Get-Item HKCU:\SOFTWARE\Autodesk | Remove-Item -Force -Verbose  
+Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue
